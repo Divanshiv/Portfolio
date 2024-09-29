41b875a9fdf8769de7bf07e465f6c81034ea7858
Divanshiv kumar <Divanshivkumar.7@gmail.com>
2024-09-29 23:31:56 +0530

    Portfolio home and about page is done

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text", {
+    strings: ["Front-End Developer", "Web Developer", "IT Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
+ 
+var typed = new Typed(".mern", {
+    strings: ["[MERN STACK]"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
+ 
